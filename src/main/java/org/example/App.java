3d522959ef6d797f4c0cb0664335e2dcc88194c1
AlexeyGulis <kiri.dh@yandex.ru>
2022-07-18 14:43:59 +0300

    lesson 50

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try{
+            session.beginTransaction();
+            session.createQuery("delete Person where age < 30").executeUpdate();
+            List<Person> people = session.createQuery("from Person").getResultList();
+            for (Person p : people
+                 ) {
+                System.out.println(p);
+            }
+        }finally {
+            session.close();
+        }
+    }
+}
